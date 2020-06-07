c75e23b5c998793fdff47e7154a69679539de4c2
VeraOm <yakov_yooy@aol.com>
2020-06-07 17:09:38 +0600

    Исправление ошибки в тесте Задачи 2

--- src/test/java/ru/netology/data/UserRegistrtionDataGenerator.java
+++ src/test/java/ru/netology/data/UserRegistrtionDataGenerator.java
@@ -5,9 +5,10 @@ import com.github.javafaker.Faker;
 import java.util.Locale;
 
 public class UserRegistrtionDataGenerator {
-    private UserRegistrtionDataGenerator(){}
+    private UserRegistrtionDataGenerator() {
+    }
 
-    public static UserRegistrationData generateData(String status){
+    public static UserRegistrationData generateData(String status) {
         Faker faker = new Faker(new Locale("en"));
         return new UserRegistrationData(faker.name().username(), faker.internet().password(true), status);
     }
